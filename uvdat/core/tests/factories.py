04728cff70eb7c33884038db51fa00b44fc0bf2c
Anne Haley <anne.haley@kitware.com>
2026-08-19 15:46:37 +0000

    chore: Update user factories for tests

--- uvdat/core/tests/factories.py
+++ uvdat/core/tests/factories.py
@@ -36,7 +36,7 @@ class UserFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = User
 
-    username = factory.SelfAttribute("email")
+    username = factory.Faker("user_name")
     email = factory.Faker("safe_email")
     first_name = factory.Faker("first_name")
     last_name = factory.Faker("last_name")
@@ -46,7 +46,7 @@ class SuperUserFactory(UserFactory):
     class Meta:
         model = User
 
-    username = factory.SelfAttribute("email")
+    username = factory.Faker("user_name")
     email = factory.Faker("safe_email")
     first_name = factory.Faker("first_name")
     last_name = factory.Faker("last_name")
